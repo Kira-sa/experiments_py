6ed81dd40c4c570baab99ac42ef06ea1ba454121
Daria <dashasokol@gmail.com>
2021-06-07 16:45:08 +0500

    каша из головы

---
+++ dns-cashe/dns-evil.py
@@ -0,0 +1,141 @@
+""" 
+TODO: Написать кеширующий DNS-сервер, 
+ * отвечающий на корректные запросы по протоколу DNS (RFC 1035) корректными ответами, 
+ * данные сервер должен брать из своего кэша или (в случае устаревания или отсутствия данных в кэше) переспрашивать у указанного сервера (см. BIND forwarders).
+
+[1-7]   реализация кэша на основе query records в качестве ключа.
+
+[8-15]  парсинг пакета для помещения в кэш записей из секций AN, NS, AR 
+        (в серии запросов dig mail.ru mx; dig mxs.mail.ru второй ответ - из кеша). 
+        Самостоятельная сборка пакета с ответом.
+
+[16-20] обработка зацикливания (корректное поведение сервера в случае, если в 
+        качестве «старшего сервера» указан он же сам, или экземпляр его, запущенный на другой машине)
+
+Обязательно должно быть реализовано
+* устаревание данных в кэше (обработка TTL), при этом клиентам должны отдавать 
+    актуальное значение ttl, то есть если 10 секунд назад от форвардера получили 
+    ответ, что данные устареют через 700 секунд, то клиенту говорим, что ttl для 
+    имеющейся у нас в кэше записи равен 690
+* временный отказ вышестоящего сервера (сервер не должен терять работоспособность 
+    (уходить в бесконечное ожидание, падать с ошибкой и т.д.), если старший 
+    сервер почему-то не ответил на запрос к нему)
+"""
+
+
+
+import argparse
+import threading
+import socket
+import queue
+import time
+import select
+import sqlite3
+import binascii
+
+dns = (('8.8.8.8',53), )
+
+
+def parse_args():
+    """ Проверка входных аргументов """
+    parser = argparse.ArgumentParser(description='dns-cache py app')
+    parser.add_argument('-p', '--port', type=int, default=53,  help='Прослушиваемый порт')
+    parser.add_argument('-f', '--forwarder', type=str,  default=['8.8.8.8:53'],  help='ip-адрес или символьное имя форвардера, c портом или без')
+
+    return parser.parse_args().__dict__
+
+
+def start(port: int, forwarder:str):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('localhost', port))
+    s.settimeout(1)
+
+    sntp = DNSServer(s, forwarder)
+    sntp.run()
+
+
+class Cache():
+    def __init__(self,filename=':memory:') -> None:
+        self.db = sqlite3.connect(filename, isolation_level=None)
+        self.db.text_factory = str
+        cursor = self.db.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS T_CACHE (K BLOB PRIMARY KEY,V BLOB)')
+        cursor.execute('PRAGMA journal_mode = off')
+        cursor.close()
+        pass
+
+    def get(self):
+        cursor = self.db.cursor()
+        return None
+    
+    def put(self, K, V):
+        cursor = self.db.cursor()
+        pass
+
+class DNSServer:
+    """ 
+    * слушать порт
+    * получив запрос проверить кэш - если в нём есть инфо - ответить по запросу,
+        если нет - спросить у старшего, сохранить ответ, ответить по запросу
+    * многопоточность (1 слушать, 1 отвечать)
+    * если указанный старший не ответил, проверить у дефолтного или сообщить об ошибке
+
+    """
+    def __init__(self, s, forwarder) -> None:
+        self.s = s
+        self.forwarder = forwarder
+        self.task = queue.Queue()
+    
+    def run(self):
+        print("DNS server start")
+        receiver = threading.Thread(target=self.listen)
+        receiver.start()
+        sender = threading.Thread(target=self.response)
+        sender.start()
+
+    def listen(self):
+        while True:
+            timeout = 3
+            ready_to_read, _, _ = select.select([self.sock], [], [], timeout)
+            if ready_to_read:
+                self.add_task(ready_to_read)
+
+    def add_task(self, ready_to_read):
+        for client in ready_to_read:
+            request, addr = client.recvfrom(1024)
+            print("Connected: {}".format(addr[0]))
+            self.task.put((request, addr, time.time()))
+
+    def response(self):
+        while True:
+            try:
+                # if self.task.empty():
+                    # time.sleep(1)
+                req, req_addr, req_time = self.task.get(timeout=1)
+                request = DNSpack()
+                # request.parse_package(req)  # разбираем запрос клиента
+                response = self.prepare_response(request, req_time).create_package() # готовим ответ
+                self.sock.sendto(response, req_addr)  # отправляем
+            except queue.Empty:
+                continue
+    
+    def prepare_response(self):
+        pass
+
+    def parse_request(self):
+        pass
+
+    
+class DNSpack():
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    try:
+        args = parse_args()
+        start(**args)
+
+    except KeyboardInterrupt:
+        print('\nTerminated.')
+        exit()
---
+++ dns-cashe/raw_.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# https://github.com/creac/dnsAgent/blob/master/dnsAgent.py
+import sys
+import os
+import logging
+import socket
+import struct
+import SocketServer
+import sqlite3
+try:
+    import gevent.monkey
+except ImportError:
+    gevent = None
+
+dns = (
+        ('8.8.8.8',53),
+        ('8.8.4.4',53),
+)
+
+pidfile = '/var/run/dnsAgent.pid'
+
+logging.basicConfig(
+        level    = logging.DEBUG,
+        filename = '/tmp/dnsAgent.log',
+        filemode = 'a',
+        datefmt  = '%Y-%m-%d %H:%M:%S',
+        format   = '%(asctime)s %(levelname)-8s %(message)s',
+)
+
+class cache(object):
+    def __init__(self,filename=':memory:'):
+        self.db = sqlite3.connect(filename,isolation_level=None)
+        self.db.text_factory = str
+        cursor = self.db.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS T_CACHE (K BLOB PRIMARY KEY,V BLOB)')
+        cursor.execute('PRAGMA journal_mode = off')
+        cursor.close()
+    def get(self,K):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute('SELECT V FROM T_CACHE WHERE K = ?',(K,))
+            return cursor.fetchall()[0][0]
+        except IndexError:
+            pass
+        finally:
+            cursor.close()
+    def put(self,K,V):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute('INSERT INTO T_CACHE (K,V) VALUES (?,?)',(K,V))
+        except sqlite3.IntegrityError:
+            cursor.execute('UPDATE T_CACHE SET V = ? WHERE K = ?',(V,K))
+        finally:
+            cursor.close()
+
+class DNSServer(SocketServer.BaseRequestHandler):
+    allow_reuse_address = True
+    daemon_threads = True
+    dns_cache = cache()
+
+    def handle(self):
+        data,sk = self.request
+        if not data:return
+        response = self._query(data)
+        if response:sk.sendto(response,self.client_address)
+
+    def _query(self,data):
+        ID,K = data[:2],data[2:]
+        V = self.dns_cache.get(K)
+        if V:return ''.join([ID,V])
+        query = ''.join([struct.pack('>H',len(data)),data])
+        for s in dns:
+            sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            sk.settimeout(5)
+            try:
+                sk.connect(s)
+                sk.send(query)
+                response = sk.recv(2048)
+            except Exception as e:
+                logging.error('connect %s:%i error:%s'%(s[0],s[1],e))
+                response = None
+            finally:
+                sk.close()
+            if response:
+                self.dns_cache.put(K,response[4:])
+                return response[2:]
+
+if __name__ == '__main__':
+    try:
+        pid = os.fork()
+        if pid > 0:sys.exit(0)
+        elif pid == 0:
+            os.chdir('/')
+            os.umask(0)
+            os.setsid()
+            pid = os.fork()
+            if pid > 0:sys.exit(0)
+    except OSError as e:
+        sys.stderr.write('fork error:%s\n'%e)
+        sys.exit(1)
+    pid = os.getpid()
+    logging.info('start daemon, pid:%i'%pid)
+    with open(pidfile,'w') as pf:pf.write(str(pid))
+    try:
+        gevent.monkey.patch_all(dns=gevent.version_info[0] >= 1)
+        logging.info('Using gevent')
+    except:
+        logging.info('Using thread')
+    try:
+        server = SocketServer.ThreadingUDPServer(('localhost', 53), DNSServer)
+        server.serve_forever()
+        server.shutdown()
+    except Exception as e:
+        logging.warn('daemon has exited: %s'%e)
+    finally:
+        os.remove(pidfile)
